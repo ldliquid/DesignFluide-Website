a0797478a85e542cbb457b292dc8db503187e101
louisedrulhe <louise.drulhe@gmail.com>
2014-05-26 12:12:13 +0200

    ajout fichiers

--- fluide.php
+++ fluide.php
@@ -19,6 +19,25 @@
   <footer>Share</footer>
 </div>
 
+<div class="bubble" id="b3">
+  <header>La communication <a href="#flute">Flute :D</a> massive des sociétés de matériel informatique a tendance à repousser les initiatives libres en marge de la société.
+  </header>
+  <div class="inner">
+    <p>le Prout (pʁut en API) (ou Prut, ou encore Pruth), une rivière qui délimite la frontière entre la République de Moldavie et la Roumanie, et se jette dans le Danube à Giurgiuleşti : Prout est une transcription commune en français de Прут, nom russe et ukrainien de la rivière, Prut est le nom roumain ainsi qu’une transcrition plus savante du russe et de l’ukrainien et Pruth une forme ancienne
+    </p>
+  </div>
+  <footer>Share</footer>
+</div>
+
+<div class="bubble" id="b4">
+  <header>La communication <a href="#flute">Flute :D</a> massive des sociétés de matériel informatique a tendance à repousser les initiatives libres en marge de la société.
+  </header>
+  <div class="inner">
+    <p>le Prout (pʁut en API) (ou Prut, ou encore Pruth), une rivière qui délimite la frontière entre la République de Moldavie et la Roumanie, et se jette dans le Danube à Giurgiuleşti : Prout est une transcription commune en français de Прут, nom russe et ukrainien de la rivière, Prut est le nom roumain ainsi qu’une transcrition plus savante du russe et de l’ukrainien et Pruth une forme ancienne
+    </p>
+  </div>
+  <footer>Share</footer>
+</div>
 
 
 
