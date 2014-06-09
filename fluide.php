5ce853e9b5198e868806c8f7d8c8d21b62cc217f
Raphaël Bastide <raphael.bastide@gmail.com>
2014-06-09 22:29:40 +0200

    Text edition

--- fluide.php
+++ fluide.php
@@ -14,7 +14,7 @@
 <div class="bubble" id="b1">
   <header>
     <span class="lien-int"><a href="#open-source">Open Source</a></span>
-    <span class="modifier"><a href="#open-source">Modifier</a></span>
+    <span class="modifier"><a target="_blank" href="https://titanpad.com/designfluideb1">Modifier</a></span>
     <span class="discussion"><a href="#open-source">Discussion</a></span>
     <span class="partager"><a href="#open-source">Partager</a></span>
 <p>Pour le <a href= "https://twitter.com/hashtag/designfluide?src=hash" target="_blank">#designfluide</a>, le code fait partie du processus créatif au même titre que l'impression #cssprint <a href= "http://stdin.fr/Works/Balsamine?action=imgtpl&G=12&upname=Balsamine_1316139743142.png" target="_blank">Identité du théatre de la balsamine</a> par OSP</p> 
@@ -24,7 +24,7 @@
 <div class="bubble" id="b2">
   <header>
     <span class="lien-int"><a href="#open-source">Open Source</a></span>
-    <span class="modifier"><a href="#open-source">Modifier</a></span>
+    <span class="modifier"><a target="_blank" href="https://titanpad.com/designfluideb2">Modifier</a></span>
     <span class="discussion"><a href="#open-source">Discussion</a></span>
     <span class="partager"><a href="#open-source">Partager</a></span>  
 <p>"Le Web ce n'est pas l'Internet."<a href="http://www.theguardian.com/technology/2014/mar/09/25-years-web-tim-berners-lee" target="_blank">25 chôses que vous ne savez pas sur le web</a></p>
@@ -34,7 +34,7 @@
 <div class="bubble" id="b3">
   <header>
     <span class="lien-int"><a href="#espace-internet">Espace d'Internet</a></span>
-    <span class="modifier"><a href="#open-source">Modifier</a></span>
+    <span class="modifier"><a target="_blank" href="https://titanpad.com/designfluideb3">Modifier</a></span>
     <span class="discussion"><a href="#open-source">Discussion</a></span>
     <span class="partager"><a href="#open-source">Partager</a></span>  
       <p>
@@ -46,7 +46,7 @@
 <div class="bubble" id="b4">
   <header>
     <span class="lien-int"><a href="#open-source">Open Source</a></span>
-    <span class="modifier"><a href="#open-source">Modifier</a></span>
+    <span class="modifier"><a target="_blank" href="https://titanpad.com/designfluideb4">Modifier</a></span>
     <span class="discussion"><a href="#open-source">Discussion</a></span>
     <span class="partager"><a href="#open-source">Partager</a></span>  
       <p>Le <a href= "https://twitter.com/hashtag/designfluide?src=hash" target="_blank">#designfluide</a> donne accès aux sources de sa création en vue de participer à l'élaboration d'un écosystème créatif</p>
@@ -56,7 +56,7 @@
 <div class="bubble" id="b5">
   <header>
     <span class="lien-int"><a href="#biens-communs">Biens communs</a></span>
-    <span class="modifier"><a href="#open-source">Modifier</a></span>
+    <span class="modifier"><a target="_blank" href="https://titanpad.com/designfluideb5">Modifier</a></span>
     <span class="discussion"><a href="#open-source">Discussion</a></span>
     <span class="partager"><a href="#open-source">Partager</a></span>  
       <p>Le <a href= "https://twitter.com/hashtag/designfluide?src=hash" target="_blank">#designfluide</a> utilise des licences libres pour encourager la libre circulation des connaissances et des créations</p>
@@ -66,7 +66,7 @@
 <div class="bubble" id="b6">
   <header>
     <span class="lien-int"><a href="#open-source">Open Source</a></span>
-    <span class="modifier"><a href="#open-source">Modifier</a></span>
+    <span class="modifier"><a target="_blank" href="https://titanpad.com/designfluideb6">Modifier</a></span>
     <span class="discussion"><a href="#open-source">Discussion</a></span>
     <span class="partager"><a href="#open-source">Partager</a></span>  
 <p>Le <a href= "https://twitter.com/hashtag/designfluide?src=hash" target="_blank">#designfluide</a> milite pour le maintien d'Internet en tant qu'espace de libertés notamment par l'utilisation d'outils libres ou par une production graphique engagée</p>
@@ -76,7 +76,7 @@
 <div class="bubble" id="b7">
   <header>
     <span class="lien-int"><a href="#meme">Mème Internet</a></span>
-    <span class="modifier"><a href="#open-source">Modifier</a></span>
+    <span class="modifier"><a target="_blank" href="https://titanpad.com/designfluideb7">Modifier</a></span>
     <span class="discussion"><a href="#open-source">Discussion</a></span>
     <span class="partager"><a href="#open-source">Partager</a></span>  
 <p>Le <a href= "https://twitter.com/hashtag/designfluide?src=hash" target="_blank">#designfluide</a> subdivise sa création graphique sous forme de données en vue de s'infiltrer dans l'océan informationnel d'Internet</p>
@@ -86,7 +86,7 @@
 <div class="bubble" id="b8">
   <header>
     <span class="lien-int"><a href="#meme">Mème Internet</a></span>
-    <span class="modifier"><a href="#open-source">Modifier</a></span>
+    <span class="modifier"><a target="_blank" href="https://titanpad.com/designfluideb8">Modifier</a></span>
     <span class="discussion"><a href="#open-source">Discussion</a></span>
     <span class="partager"><a href="#open-source">Partager</a></span>  
 <p>Le <a href= "https://twitter.com/hashtag/designfluide?src=hash" target="_blank">#designfluide</a> subdivise sa création graphique sous forme de données en vue de s'infiltrer dans l'océan informationnel d'Internet</p>
@@ -96,7 +96,7 @@
 <div class="bubble" id="b9">
   <header>
     <span class="lien-int"><a href="#meme">Mème Internet</a></span>
-    <span class="modifier"><a href="#open-source">Modifier</a></span>
+    <span class="modifier"><a target="_blank" href="https://titanpad.com/designfluideb9">Modifier</a></span>
     <span class="discussion"><a href="#open-source">Discussion</a></span>
     <span class="partager"><a href="#open-source">Partager</a></span>  
 <p>Le <a href= "https://twitter.com/hashtag/designfluide?src=hash" target="_blank">#designfluide</a> utilise la propagation comme système de communication</p>
@@ -106,7 +106,7 @@
 <div class="bubble" id="b10">
   <header>
     <span class="lien-int"><a href="#web-semantique">Web sémantique</a></span>
-    <span class="modifier"><a href="#open-source">Modifier</a></span>
+    <span class="modifier"><a target="_blank" href="https://titanpad.com/designfluideb10">Modifier</a></span>
     <span class="discussion"><a href="#open-source">Discussion</a></span>
     <span class="partager"><a href="#open-source">Partager</a></span>  
 <p>Le <a href= "https://twitter.com/hashtag/designfluide?src=hash" target="_blank">#designfluide</a> utilise la sémantique pour connecter les idées entre elles et encourager le potentiel cognitif du web des données</p>
@@ -116,7 +116,7 @@
 <div class="bubble" id="b11">
   <header>
     <span class="lien-int"><a href="#web-semantique">Web sémantique</a></span>
-    <span class="modifier"><a href="#open-source">Modifier</a></span>
+    <span class="modifier"><a target="_blank" href="https://titanpad.com/designfluideb11">Modifier</a></span>
     <span class="discussion"><a href="#open-source">Discussion</a></span>
     <span class="partager"><a href="#open-source">Partager</a></span>  
 <p>Le <a href= "https://twitter.com/hashtag/designfluide?src=hash" target="_blank">#designfluide</a> crée des architectures fluides : des structures extrêmement malléables, liquides, capables de supporter toutes sortes de contenus</p>
@@ -126,7 +126,7 @@
 <div class="bubble" id="b12">
   <header>
     <span class="lien-int"><a href="#metadonnees">Métadonnées</a></span>
-    <span class="modifier"><a href="#open-source">Modifier</a></span>
+    <span class="modifier"><a target="_blank" href="https://titanpad.com/designfluideb12">Modifier</a></span>
     <span class="discussion"><a href="#open-source">Discussion</a></span>
     <span class="partager"><a href="#open-source">Partager</a></span>  
 <p>Le <a href= "https://twitter.com/hashtag/designfluide?src=hash" target="_blank">#designfluide</a> utilise les métadonnées pour produire une création ayant une infinité de forme différentes et ainsi recréer la notion de fluidité</p>
@@ -206,21 +206,9 @@
  <div class="bubble" id="b20">
   <header>
     <span class="lien-int"><a href="#open-source">Open Source</a></span>
-    <span class="modifier"><a href="#open-source">Modifier</a></span>
+    <span class="modifier"><a target="_blank" href="https://titanpad.com/designfluideb6">Modifier</a></span>
     <span class="discussion"><a href="#open-source">Discussion</a></span>
     <span class="partager"><a href="#open-source">Partager</a></span>  
 <p>Les trois mots "Examiner, apprendre, connaitre" résument le mécanisme de la pratique libre. Mais, est ce que la première étape du processus créatif ne serait-elle pas basée, elle aussi, sur ces trois notions ?</p>
   </header>  
 </div> 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-
-
